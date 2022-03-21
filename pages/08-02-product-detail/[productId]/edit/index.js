8820722d2a116b2643acb840a1f7b5e76e7d729a
Eunjeong Noh <nej1044@naver.com>
2022-03-21 12:23:53 +0900

    2022/Mar/21-day08

--- pages/08-02-product-detail/[productId]/edit/index.js
+++ pages/08-02-product-detail/[productId]/edit/index.js
@@ -2,9 +2,9 @@ import {gql, useMutation} from '@apollo/client'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
-const CREATE_PRODUCT = gql`
-  mutation createProduct($seller: String, $createProductInput: CreateProductInput!) {
-    createProduct(seller: $seller, createProductInput: $createProductInput) {
+const UPDATE_PRODUCT = gql`
+  mutation updateProduct($productId: ID, $updateProductInput: UpdateProductInput!) {
+    updateProduct(productId: $productId, updateProductInput: $updateProductInput) {
       _id
       number
       message
@@ -12,9 +12,9 @@ const CREATE_PRODUCT = gql`
   }
 `
 
-const ProductNewPage=()=> {
+const ProductUpdatePage=()=> {
   const router = useRouter()
-  const [createProduct] = useMutation(CREATE_PRODUCT)
+  const [updateProduct] = useMutation(UPDATE_PRODUCT)
   const [seller, setSeller] = useState('')
   const [name, setName] = useState('')
   const [detail, setDetail] = useState('')
@@ -36,28 +36,29 @@ const ProductNewPage=()=> {
     setPrice(Number(event.target.value))
   }
 
-  const onClickSubmit =async()=> {
-    const result = await createProduct({
-      variables: {seller, createProductInput: {
+  const onClickUpdate =async()=> {
+    const result = await updateProduct({variables: {
+      productId: router.query.productId,
+      updateProductInput: {
         name,
         detail,
         price
-      }}
-    })
-    router.push(`08-02-product-detail/${result.data?.createProduct._id}`)
+      }
+    }})
+    router.push(`/08-02-product-detail/${result.data?.updateProduct._id}`)
   }
     return(
       <>
         <div>
-          <h1>등록페이지</h1>
+          <h1>수정페이지</h1>
           판매자 <input type="text" onChange={onChangeSeller} /><br />
           제품명 <input type="text" onChange={onChangeName} /><br />
           상세내용 <input type="text" onChange={onChangeDetail} /><br />
           가격 <input type="number" onChange={onChangPrice} /><br />
-          <button onClick={onClickSubmit}>등록하기</button>
+          <button onClick={onClickUpdate}>수정하기</button>
         </div>
       </>
     )
   }
   
-  export default ProductNewPage
+  export default ProductUpdatePage
