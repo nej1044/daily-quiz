b125a080fe20bfb2b366900872cecfec6c22cbf7
Eunjeong Noh <nej1044@naver.com>
2022-04-25 12:48:49 +0900

    2022/Apr/25-day30

---
+++ src/commons/libraries/getAccessToken.tsx
@@ -0,0 +1,25 @@
+import { GraphQLClient, gql } from "graphql-request";
+
+const RESTORE_ACCESS_TOKEN = gql`
+  mutation restoreAccessToken {
+    restoreAccessToken {
+      accessToken
+    }
+  }
+`;
+
+const getAccessToken = async () => {
+  try {
+    const graphQLClient = new GraphQLClient(
+      "https://backend06.codebootcamp.co.kr/graphql",
+      { credentials: "include" }
+    );
+    const result = await graphQLClient.request(RESTORE_ACCESS_TOKEN);
+    const newAccessToken = result.restoreAccessToken.accessToken;
+    return newAccessToken;
+  } catch (error) {
+    if (error instanceof Error) console.log(error);
+  }
+};
+
+export default getAccessToken;
--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -7,17 +7,51 @@ import {
 import { useRecoilState } from "recoil";
 import { globalToken } from "../../../../pages/_app";
 import { createUploadLink } from "apollo-upload-client";
+import { useEffect } from "react";
+
+import { onError } from "@apollo/client/link/error";
+import getAccessToken from "../../../commons/libraries/getAccessToken";
 
 const ApolloSetting = (props) => {
-  const [accessToken] = useRecoilState(globalToken);
+  const [accessToken, setAccessToken] = useRecoilState(globalToken);
+
+  useEffect(() => {
+    // const accessToken = localStorage.getItem("accessToken");
+    // const userInfo = JSON.parse(localStorage.getItem("userInfo") || "{}");
+    // setUserInfo(userInfo);
+    getAccessToken().then((newAccessToken) => {
+      setAccessToken(newAccessToken);
+    });
+  }, []);
+
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        if (err.extensions.code === "UNAUTHENTICAED") {
+          getAccessToken().then((newAccessToken) => {
+            setAccessToken(newAccessToken);
+
+            operation.setContext({
+              headers: {
+                ...operation.getContext().headers,
+                Authorization: `Bearer ${newAccessToken}`,
+              },
+            });
+            return forward(operation);
+          });
+        }
+      }
+    }
+  });
 
   const uploadLink = createUploadLink({
-    uri: "http://backend06.codebootcamp.co.kr/graphql",
+    uri: "https://backend06.codebootcamp.co.kr/graphql",
     headers: { Authorization: `Bearer ${accessToken}` },
+    credentials: "include",
   });
 
   const client = new ApolloClient({
-    link: ApolloLink.from([uploadLink]),
+    link: ApolloLink.from([errorLink, uploadLink as unknown as ApolloLink]),
     cache: new InMemoryCache(),
   });
 
